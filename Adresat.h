e4128b3b9c62744561c5cb8c2294c3fdfd43c06b
Adam Macura <adam.macura.programista@gmail.com>
2023-06-01 21:31:54 +0200

    Add contructor to Adresat class

--- Adresat.h
+++ Adresat.h
@@ -17,6 +17,15 @@ class Adresat{
 
 
 public:
+    Adresat(int id = 0, string imie = "", string nazwisko = "", string numerTelefonu = "", string email= "", string adres = ""){
+        this -> id = id;
+        this -> idUzytkownika = idUzytkownika;
+        this -> nazwisko = nazwisko;
+        this -> numerTelefonu = numerTelefonu;
+        this -> email = email;
+        this -> adres = adres;
+    }
+
     void ustawId(int noweId);
     void ustawIdUzytkownika(int noweIdUzytkownika);
     void ustawImie(string noweImie);
